0a7e08ebe53b5ca4a2b0039307c941f00d1985ca
linneamunro <45132398+linneamunro@users.noreply.github.com>
2021-09-18 12:29:36 -0700

    Update main.js

--- main.js
+++ main.js
@@ -220,7 +220,7 @@ function waitInput(id, time){
   setTimeout(enableInput, time, id);
 }
 function showVideo(){
-  show('smokeVid'); 
+  show('introVid'); 
   waitShowNext(23000); 
   hide('beginbox'); 
   hide('intro'); 
