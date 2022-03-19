4a8b693a19f7d74c086b14bf3e1740d9f69e66a4
linneamunro <45132398+linneamunro@users.noreply.github.com>
2022-03-18 18:33:29 -0700

    Update main.js

--- main.js
+++ main.js
@@ -301,7 +301,7 @@ function showStoryVideo(){
 
 function showAfVideo(){
   show('africaVid');
-  waitShowNext(150000); 
+  waitShowNext2(150000); 
   hide('beginbox'); 
   hide('intro'); 
   hide('coverPic'); 
